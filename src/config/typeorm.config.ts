c57a9d9073f255358899ed62fe9dd670df115fe6
Levi Liester <levi.liester@gmail.com>
2020-12-23 00:58:01 -0600

    Adding some SSL nonsense for postgres

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,13 @@
 import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 
+const TYPEORMEXTRA = process.env.NODE_ENV === 'production' ? {extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    }}: null;
+
+console.log(`TYPEORMEXTRA: ${JSON.stringify(TYPEORMEXTRA)}`)
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     host: process.env.DATABASE_HOST || 'localhost',
@@ -9,6 +17,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     database: process.env.DATABASE_NAME || 'landlord',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
-    ssl: true
+    ...TYPEORMEXTRA
 }
-
