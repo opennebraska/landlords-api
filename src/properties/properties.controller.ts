f3fe82fddeda2f1e26a6539900cf5589da360769
dodgees <dodgees2@gmail.com>
2021-01-05 22:08:16 -0600

    Cleanup commented code and rename controller method.

--- src/properties/properties.controller.ts
+++ src/properties/properties.controller.ts
@@ -1,13 +1,9 @@
-import {Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards, UsePipes, ValidationPipe} from '@nestjs/common';
-import {PropertiesService} from "./properties.service";
-import {CreatePropertyDto} from "./dto/create.property.dto";
-import {MealStatusValidationPipe} from "./pipes/meal-status-validation.pipe";
-import {GetPropertiesFilterDto} from "./dto/get-properties-filter.dto";
-import {Property} from "./property.entity";
-import {MealStatus} from "./property-status.enum";
-import {AuthGuard} from "@nestjs/passport";
-import {GetUser} from "../auth/get-user.decorator";
-import {User} from "../auth/user.entity";
+import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
+import { PropertiesService } from './properties.service';
+import { GetPropertiesFilterDto } from './dto/get-properties-filter.dto';
+import { Property } from './property.entity';
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from '../auth/user.entity';
 
 @Controller('properties')
 // @UseGuards(AuthGuard())
@@ -16,44 +12,14 @@ export class PropertiesController {
     }
 
     @Get()
-    getMeals(
+    getProperties(
         @Query(ValidationPipe) filterDto: GetPropertiesFilterDto,
         @GetUser() user: User
     ): Promise<Property[]> {
         return this.propertiesService.getProperties(filterDto, user)
     }
 
-    // @Get('/:id')
-    // getMealById(
-    //     @Param('id', ParseIntPipe) id: number,
-    //     @GetUser() user: User
-    // ): Promise<Meal> {
-    //     return this.mealsService.getMealById(id, user);
-    // }
-    //
-    // @Post()
-    // @UsePipes(ValidationPipe)
-    // createMeal(
-    //     @Body() createMealDto: CreatePropertyDto,
-    //     @GetUser() user: User,
-    // ): Promise<Meal> {
-    //     return this.mealsService.createMeal(createMealDto, user);
-    // }
 
-    // @Delete('/:id')
-    // deleteMealById(
-    //     @Param('id', ParseIntPipe) id: number,
-    //     @GetUser() user: User,
-    // ): Promise<void> {
-    //     return this.mealsService.deleteMeal(id, user);
-    // }
 
-    // @Patch('/:id/status')
-    // updateMealById(
-    //     @Param('id', ParseIntPipe) id: number,
-    //     @Body('status', MealStatusValidationPipe) status: MealStatus,
-    //     @GetUser() user: User,
-    // ): Promise<Meal> {
-    //     return this.mealsService.updateMealStatus(id, status, user)
-    // }
+
 }
--- src/properties/properties.repository.ts
+++ src/properties/properties.repository.ts
@@ -7,31 +7,9 @@ import {User} from "../auth/user.entity";
 @EntityRepository(Property)
 export class PropertiesRepository extends Repository<Property> {
     async createProperty(createMealDto: CreatePropertyDto, user: User): Promise<Property> {
-        const {
-            digestion,
-            feeling,
-            hungerRatingBefore,
-            mood,
-            satietyRatingAfter,
-            thinking,
-            time,
-            whatDidYouDrink,
-            whatDidYouEat
-        } = createMealDto;
         const property = new Property();
-        // meal.digestion = digestion;
-        // meal.feeling = feeling;
-        // meal.hungerRatingBefore = hungerRatingBefore;
-        // meal.mood = mood;
-        // meal.satietyRatingAfter = satietyRatingAfter;
-        // meal.thinking = thinking;
-        // meal.time = time;
-        // meal.whatDidYouDrink = whatDidYouDrink;
-        // meal.whatDidYouEat = whatDidYouEat;
-        // meal.user = user;
         await property.save();
 
-        // delete meal.user;
         return property;
     }
 
