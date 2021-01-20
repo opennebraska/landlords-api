95a79faee28353ef8c4f571a2482e68f6a0656fa
Levi Liester <levi.liester@gmail.com>
2021-01-19 22:11:04 -0600

    Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {typeOrmConfig} from "./config/typeorm.config";
 import { AuthModule } from './auth/auth.module';
 import {PropertiesModule} from "./properties/properties.module";
+import {SheltersModule} from "./shelters/shelters.module";
 
 @Module({
     imports: [
         PropertiesModule,
+        SheltersModule,
         TypeOrmModule.forRoot(typeOrmConfig),
         AuthModule
     ],
